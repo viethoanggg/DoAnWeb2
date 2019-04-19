c9a2d89e1dcaa7e52dee79fae3055cfb6961cbe9
Ninh Ngoc Hieu <ninhngochieu@users.noreply.github.com>
2019-04-19 23:27:20 +0700

    Update cart.php

--- Web2/php/cart.php
+++ Web2/php/cart.php
@@ -4,12 +4,25 @@
 	 */
 	class cart
 	{
-		
+		/*Lấy masachn từ chọn mua*/
 		public static function addcart(){
 			require('DataProvider.php');
 			if(isset($_GET['masach']))
 				$MaSach=$_GET['masach'];
-			echo $MaSach;
+			/*Khởi tạo session*/
+			if(isset($MaSach)){
+				if(isset($_SESSION['cart'])){//Kiểm tra giỏ hàng đã tồn tại chưa
+				if(isset($_SESSION['cart'][$MaSach])){//Tồn tại mã sách này thì tăng lên 1
+					$_SESSION['cart'][$MaSach]['sl']+=1;
+				}
+				else{
+					$_SESSION['cart'][$MaSach]['sl'] = 1;//Chưa tồn tại mã sách này thì tạo giá trị ban đầu cho nó
+				}
+			}
+			else{//Chưa tồn tại thì tạo session với sl=1
+				$_SESSION['cart'][$MaSach]['sl'] = 1;
+			}
 		}
 	}
+}
 ?>
