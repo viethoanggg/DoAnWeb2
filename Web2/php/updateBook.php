d9da18b8a1f96af8c52f3476d8d899a9e61228f4
viethoanggg <49758848+viethoanggg@users.noreply.github.com>
2019-04-20 17:06:54 +0700

    Update updateBook.php

--- Web2/php/updateBook.php
+++ Web2/php/updateBook.php
@@ -11,11 +11,36 @@
 		.	"MaTheLoai='".$_POST['matheloai']."', "
 		.	"TenSach='".$_POST['tensach']."', "
 		.	"TenTacGia='".$_POST['tentacgia']."', ";
-		if(isset($_POST['hinhanh']))
-			$sql=$sql .	"HinhAnh='".$_POST['hinhanh']."', ";
+		if(isset($_FILES['hinhanh']))
+			$sql=$sql .	"HinhAnh='".$_FILES['hinhanh']['name']."', ";
 		$sql=$sql .	"Gia='".(int)$_POST['gia']."' "
 		.	"WHERE MaSach='".$_POST['masach']."'";
 		DataProvider::executeQuery($sql);
+
+//------------------------------------------------Xử lý chuyển file ảnh-------------------------------------------------------------------//	
+		
+		if(isset($_FILES['hinhanh']) && $_FILES['hinhanh']['size'] < 2000000 )
+		{	
+			$tmp_name=$_FILES['hinhanh']['tmp_name'];
+			$matheloai=$_POST['matheloai'];
+			if($matheloai=="NN")
+				$imageURL="../images/ngoaingu/".$_FILES['hinhanh']['name'];
+			else if($matheloai=="KT")
+				$imageURL="../images/kinhte/".$_FILES['hinhanh']['name'];
+			else if($matheloai=="KNS")
+				$imageURL="../images/kynangsong/".$_FILES['hinhanh']['name'];
+			else if($matheloai=="LS")
+				$imageURL="../images/lichsu/".$_FILES['hinhanh']['name'];
+			else if($matheloai=="CN")
+				$imageURL="../images/chuyennganh/".$_FILES['hinhanh']['name'];
+			else if($matheloai=="TN")
+				$imageURL="../images/thieunhi/".$_FILES['hinhanh']['name'];
+			else if($matheloai=="TT")
+				$imageURL="../images/tuoiteen/".$_FILES['hinhanh']['name'];
+			else if($matheloai=="VH")
+				$imageURL="../images/vanhoc/".$_FILES['hinhanh']['name'];
+			move_uploaded_file($tmp_name,$imageURL);
+		}
 		
 		//------------------------Sua trong database chi tiet sach-----------------------------------------------//
 		
