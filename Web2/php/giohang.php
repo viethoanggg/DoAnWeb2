bd5c5d74a167f56d81144a161f666e355993c070
Ninh Ngoc Hieu <ninhngochieu@users.noreply.github.com>
2019-04-20 14:39:31 +0700

    Update giohang.php

--- Web2/php/giohang.php
+++ Web2/php/giohang.php
@@ -331,7 +331,6 @@
 		<p>- Ra đời từ năm 2011, đến nay Onepiece.vn đã trở thành địa chỉ mua sách online quen thuộc của hàng ngàn độc giả trên cả nước. Với đầu sách phong phú, thuộc các thể loại: Văn học nước ngoại, Văn học trong nước, Kinh tế, Kỹ năng sống, Thiếu nhi, Sách học ngoại ngữ, Sách chuyên ngành,... được cập nhật liên tục từ các nhà xuất bản uy tín trong nước. </p><br>
 		<p>- Khi mua sách online tại Onepiece.vn, Quý khách được Bọc plastic miễn phí đến 99% (trừ sách bìa cứng, sách dạng hộp - dạng đặc biệt, sách khổ quá to, ...)</p><br>
 		<p>- Ngoài ra, với hình thức Giao hàng thu tiền tận nơi và Đổi hàng trong vòng 7 ngày nếu sách có bất kỳ lỗi nào trong quá trình in ấn sẽ giúp Quý khách yên tâm hơn khi mua sắm tại Onepiece.vn</p>
-
 	</div>
 	<hr style="border:1px solid black;">
 	<div class="chitietfooter">
@@ -359,6 +358,5 @@
 	</div>
 </div>
 </div>
-
 </body>
 </html>
