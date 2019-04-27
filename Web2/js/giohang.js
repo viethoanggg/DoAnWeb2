837aa945f79042f0770e9c211a04fa3d8f33b56d
Ninh Ngoc Hieu <ninhngochieu@users.noreply.github.com>
2019-04-27 23:09:47 +0700

    update giỏ hàng khi xôa hết sảm phẩm

--- Web2/js/giohang.js
+++ Web2/js/giohang.js
@@ -31,11 +31,20 @@ function xoasp(masach){
 			}
 		}).done(function(data){
 			// console.log(data);
-			debugger;
-			alert(JSON.parse(data).masach);
-			alert(JSON.parse(data).sl);
-
-			$("tr#"+masach+"").hide();
+			//debugger;
+			//alert(JSON.parse(data).masach);
+			//alert(JSON.parse(data).sl);
+			//alert(JSON.parse(data).tongtien);
+			if(data!="false"){
+				$('.badge.badge-secondary ').text(JSON.parse(data).sl);
+				$('#demsl').text(JSON.parse(data).sl);
+				$('#tong').text(formatNumber(JSON.parse(data).tongtien,".",",")+"đ");
+				$('#thanhtien').text(formatNumber(JSON.parse(data).tongtien+50000,".",",")+"đ");
+				$("tr#"+masach+"").hide();
+			}
+			else{
+				document.getElementById('giohangrong').innerHTML=`<div><table class="table"><tr><td style="background-color:#ddd; height: 150px;text-align: center;padding-top:100px ">Không có sản phẩm nào trong giỏ hàng của bạn</td></tr><tr><td style="background-color:#ddd;border: 0;height: 200px;text-align: center;"><button class="thanhtoan" onclick="window.location.assign('../index.php')">Tiếp tục mua sắm</button></td></tr></table></div>`;
+			}
 		})
 	}
 }
@@ -55,4 +64,15 @@ function drop(){
 	if(document.getElementById(`soluong`).value < 2){
 		document.getElementById(`soluong`).value = 2;
 	}
+}
+function formatNumber(nStr, decSeperate, groupSeperate) {
+	nStr += '';
+	x = nStr.split(decSeperate);
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + groupSeperate + '$2');
+	}
+	return x1 + x2;
 }
