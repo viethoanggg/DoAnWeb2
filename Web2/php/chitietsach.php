a55e16db366e401d0daf11e670154b4bdd139c58
hoangtriet <45569113+hoangtriet@users.noreply.github.com>
2019-05-09 09:32:21 +0700

    Update chitietsach.php

--- Web2/php/chitietsach.php
+++ Web2/php/chitietsach.php
@@ -92,8 +92,8 @@
 				
 				<ul class="nav navbar-nav">
 					<li class="active"><a href="../index.php">Trang chủ</a></li>
-					<li><a href="#">About</a></li>
-					<li><a href="#">Contact</a></li>
+					<li><a href="About.php">About</a></li>
+					<li><a href="Contact.php">Contact</a></li>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Thể loại <span class="caret"></span></a>
 						<ul class="dropdown-menu">
