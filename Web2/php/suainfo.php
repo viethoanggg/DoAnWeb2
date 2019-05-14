9f9e9dd2c7b082de8ffe0a562e975dce651b4fc4
Ninh Ngoc Hieu <ninhngochieu@users.noreply.github.com>
2019-05-14 22:50:52 +0700

    fix

--- Web2/php/suainfo.php
+++ Web2/php/suainfo.php
@@ -1,42 +1,43 @@
 <?php
 
-	ini_set('session.auto_start',0);
-	ini_set('session.cookie_lifetime',0);
-	session_start();
-	
-	
-	require('DataProvider.php');
-	if(isset($_POST['makhachhang']))
-	{
-		$maKH=$_POST['makhachhang'];
-		
-		//------------------------Sua trong database khachhang-----------------------------------------------//
-		
-		$sql="UPDATE khachhang SET "
-		.	"HoTen='".$_POST['hoten']."', "
-		.	"Email='".$_POST['email']."', "
-		.	"SĐT='".$_POST['sdt']."'  "
-		.	"WHERE MaKH='".$_POST['makhachhang']."'";
-		DataProvider::executeQuery($sql);
-		
-		$_SESSION['login']=array(	'MaKH' => $maKH,
-									'TenDangNhap' => $_POST['tendangnhap'],
-									'MaQuyen' => $_SESSION['login']['MaQuyen'],
-									'HoTen' => $_POST['hoten'],
-									'Email' => $_POST['email'],
-									'SĐT' => $_POST['sdt'],
-									'DiaChi' => $_POST['diachi'],
-									'Phivanchuyen' => 25000);
+ini_set('session.auto_start',0);
+ini_set('session.cookie_lifetime',0);
+session_start();
 
-		
-	}
-		
+
+require('DataProvider.php');
+if(isset($_POST['makhachhang']))
+{
+	$maKH=$_POST['makhachhang'];
+
+		//------------------------Sua trong database khachhang-----------------------------------------------//
+
+	$sql="UPDATE khachhang SET "
+	.	"HoTen='".$_POST['hoten']."', "
+	.	"Email='".$_POST['email']."', "
+	.	"SĐT='".$_POST['sdt']."'  "
+	.	"WHERE MaKH='".$_POST['makhachhang']."'";
+	DataProvider::executeQuery($sql);
+
+	$_SESSION['login']=array(	'MaKH' => $maKH,
+		'TenDangNhap' => $_POST['tendangnhap'],
+		'MaQuyen' => $_SESSION['login']['MaQuyen'],
+		'HoTen' => $_POST['hoten'],
+		'Email' => $_POST['email'],
+		'SĐT' => $_POST['sdt'],
+		'DiaChi' => $_POST['diachi'],
+		'htgh' => 1,
+		'httt' => 3);
+
+
+}
+
 	//------------------------Quay ve trang quanlysanpham.php-----------------------------------------------//
-	
-	$hostName=$_SERVER['HTTP_HOST'];
-	$dirURL=rtrim(dirname($_SERVER['PHP_SELF']));
-	$pageName="giohang.php";
-	$url="http://".$hostName.$dirURL."/".$pageName;
-	header("Location:$url");
-	exit;
+
+$hostName=$_SERVER['HTTP_HOST'];
+$dirURL=rtrim(dirname($_SERVER['PHP_SELF']));
+$pageName="giohang.php";
+$url="http://".$hostName.$dirURL."/".$pageName;
+header("Location:$url");
+exit;
 ?>
